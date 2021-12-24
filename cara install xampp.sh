ffe477b4f5d60a031931521037d993dd114861d3
Faris Helmi Permana <69218443+farishelmipermana@users.noreply.github.com>
2021-12-24 11:38:25 +0700

    Add files via upload

---
+++ cara install xampp.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Path: cara install xampp.sh
+
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ ls
+binus                             DVWA-master.zip
+code_1.63.2-1639562499_amd64.deb  google-chrome-stable_current_amd64.deb
+DVWA-master                       xampp-linux-x64-8.1.1-1-installer.run
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ ^C
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ ls -al
+total 325104
+drwxr-xr-x  4 john john      4096 Des 24 11:22 .
+drwxr-xr-x 22 john john      4096 Des 24 09:52 ..
+drwxrwxr-x  2 john john      4096 Des 24 10:40 binus
+-r--------  1 john john  81096480 Des 23 16:23 code_1.63.2-1639562499_amd64.deb
+drwxrwxr-x 11 john john      4096 Des  1 15:59 DVWA-master
+-rw-rw-r--  1 john john   1383191 Des 24 11:21 DVWA-master.zip
+-r--------  1 john john  89348996 Des 23 17:22 google-chrome-stable_current_amd64.deb
+-rw-rw-r--  1 john john 161056568 Des 24 11:20 xampp-linux-x64-8.1.1-1-installer.run
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ sudo chmod +x xampp-linux-x64-8.1.1-1-installer.run
+[sudo] password for john: 
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ ls -al
+total 325104
+drwxr-xr-x  4 john john      4096 Des 24 11:22 .
+drwxr-xr-x 22 john john      4096 Des 24 09:52 ..
+drwxrwxr-x  2 john john      4096 Des 24 10:40 binus
+-r--------  1 john john  81096480 Des 23 16:23 code_1.63.2-1639562499_amd64.deb
+drwxrwxr-x 11 john john      4096 Des  1 15:59 DVWA-master
+-rw-rw-r--  1 john john   1383191 Des 24 11:21 DVWA-master.zip
+-r--------  1 john john  89348996 Des 23 17:22 google-chrome-stable_current_amd64.deb
+-rwxrwxr-x  1 john john 161056568 Des 24 11:20 xampp-linux-x64-8.1.1-1-installer.run
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ 
+
+#clear 
+john@john-Lenovo-ideapad-320-14IKB:~/Downloads$ sudo ./xampp-linux-x64-8.1.1-1-installer.run
+
+
+
